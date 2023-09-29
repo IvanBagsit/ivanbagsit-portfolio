5ed7220913d1abd5c07eb260f6ae7da6676829b1
ivanbagsit <ivan.bagsit@novare.com.hk>
2023-09-29 17:51:11 +0800

    added invalid page

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -4,6 +4,7 @@ import styles from "./Dashboard.module.css";
 import Home from "./Menu/Home";
 import About from "./Menu/About";
 import Portfolio from "./Menu/Portfolio";
+import Invalid from "./Menu/Invalid";
 
 const Dashboard = () => {
     return (
@@ -14,6 +15,7 @@ const Dashboard = () => {
                 <Route path="/home" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/*" element={<Invalid />} />
             </Routes>
         </div>
     );
